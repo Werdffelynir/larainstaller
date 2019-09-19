26af9dc61ef805a282d7115cddfd296cec377157
Werdffelynir <werdffelynir@gmail.com>
2019-09-19 15:33:31 +0300

    added vue

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Main')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <h2>Main</h2>
+    <p>This is my body content.</p>
+@endsection
